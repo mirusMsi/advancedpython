3c94e9ca0a57e1be7715f327705a38f2b874dfb7
Webmirus <miroshkin.mirus@ya.ru>
2019-09-22 18:16:32 +0300

    lesson 6

--- program/server/test/test_protocol.py
+++ program/server/test/test_protocol.py
@@ -3,139 +3,153 @@ from datetime import datetime
 from protocol import validate_request, make_response, make_200, make_400, make_404, make_500
 
 
-CODE = 200
+@pytest.fixture
+def expected_code():
+    return 200
 
-TIME = datetime.now().timestamp()
+@pytest.fixture
+def expected_action():
+    return 'test'
 
-ACTION = 'test'
+@pytest.fixture
+def expected_time():
+    return datetime.now().timestamp()
 
-DATA = 'some client data'
+@pytest.fixture
+def expected_data():
+    return 'some client data'
 
-REQUEST = {
-    'action': ACTION,
-    'time': TIME,
-    'data': DATA
-}
+@pytest.fixture
+def initial_request(expected_action, expected_time, expected_data):
+    return {
+        'action': expected_action,
+        'time': expected_time,
+        'data': expected_data
+    }
+
+@pytest.fixture
+def invalid_request(expected_data):
+    return {
+        'data': expected_data
+    }
 
 
-def test_action_make_response():
-    response = make_response(REQUEST, CODE, DATA, date=TIME)
+def test_action_make_response(initial_request, expected_code, expected_action, expected_time, expected_data):
+    response = make_response(initial_request, expected_code, expected_data, date=expected_time)
     action = response.get('action')
-    assert action == ACTION
+    assert action == expected_action
 
 
-def test_code_make_response():
-    response = make_response(REQUEST, CODE, DATA, date=TIME)
+def test_code_make_response(initial_request, expected_code, expected_time, expected_data):
+    response = make_response(initial_request, expected_code, expected_data, date=expected_time)
     code = response.get('code')
-    assert code == CODE
+    assert code == expected_code
 
 
-def test_time_make_response():
-    response = make_response(REQUEST, CODE, DATA, date=TIME)
+def test_time_make_response(initial_request, expected_code, expected_time, expected_data):
+    response = make_response(initial_request, expected_code, expected_data, date=expected_time)
     time = response.get('time')
-    assert time == TIME
+    assert time == expected_time
 
 
-def test_data_make_response():
-    response = make_response(REQUEST, CODE, DATA, date=TIME)
+def test_data_make_response(initial_request, expected_code, expected_time, expected_data):
+    response = make_response(initial_request, expected_code, expected_data, date=expected_time)
     data = response.get('data')
-    assert data == DATA
+    assert data == expected_data
 
 
-def test_invalid_make_response():
+def test_invalid_make_response(expected_code):
     with pytest.raises(AttributeError):
-        response = make_response(None, CODE)
+        response = make_response(None, expected_code)
 
 
-def test_not_action_validate_request():
-    check = validate_request({'time': TIME})
-    assert check == False
+def test_valid_validate_request(initial_request):
+    assert validate_request(initial_request)
 
 
-def test_not_time_validate_request():
-    check = validate_request({'action': ACTION})
-    assert check == False
+def test_invalid_validate_request(invalid_request):
+    assert validate_request(invalid_request) == False
 
 
-def test_action_make_200():
-    response = make_200(REQUEST, DATA, date=TIME)
+def test_action_make_200(initial_request, expected_action, expected_time, expected_data):
+    response = make_200(initial_request, expected_data, date=expected_time)
     action = response.get('action')
-    assert action == ACTION
+    assert action == expected_action
 
 
-def test_code_make_200():
-    response = make_200(REQUEST, DATA, date=TIME)
+def test_code_make_200(initial_request, expected_time, expected_data):
+    response = make_200(initial_request, expected_data, date=expected_time)
     code = response.get('code')
     assert code == 200
 
 
-def test_time_make_200():
-    response = make_200(REQUEST, DATA, date=TIME)
+def test_time_make_200(initial_request, expected_time, expected_data):
+    response = make_200(initial_request, expected_data, date=expected_time)
     time = response.get('time')
-    assert time == TIME
+    assert time == expected_time
 
 
-def test_data_make_200():
-    response = make_200(REQUEST, DATA, date=TIME)
+def test_data_make_200(initial_request, expected_time, expected_data):
+    response = make_200(initial_request, expected_data, date=expected_time)
     data = response.get('data')
-    assert data == DATA
+    assert data == expected_data
 
 
-def test_action_make_400():
-    response = make_400(REQUEST, DATA, date=TIME)
+def test_action_make_400(initial_request, expected_action, expected_time, expected_data):
+    response = make_400(initial_request, expected_data, date=expected_time)
     action = response.get('action')
-    assert action == ACTION
+    assert action == expected_action
 
 
-def test_code_make_400():
-    response = make_400(REQUEST, DATA, date=TIME)
+def test_code_make_400(initial_request, expected_time, expected_data):
+    response = make_400(initial_request, expected_data, date=expected_time)
     code = response.get('code')
     assert code == 400
 
 
-def test_time_make_400():
-    response = make_400(REQUEST, DATA, date=TIME)
+def test_time_make_400(initial_request, expected_time, expected_data):
+    response = make_400(initial_request, expected_data, date=expected_time)
     time = response.get('time')
-    assert time == TIME
+    assert time == expected_time
 
 
-def test_data_make_400():
-    response = make_400(REQUEST, DATA, date=TIME)
+def test_data_make_400(initial_request, expected_time, expected_data):
+    response = make_400(initial_request, expected_data, date=expected_time)
     data = response.get('data')
-    assert data == DATA
+    assert data == expected_data
 
 
-def test_code_make_404():
-    response = make_404(REQUEST, date=TIME)
+def test_code_make_404(initial_request, expected_time):
+    response = make_404(initial_request, date=expected_time)
     code = response.get('code')
     assert code == 404
 
 
-def test_time_make_404():
-    response = make_404(REQUEST, date=TIME)
+def test_time_make_404(initial_request, expected_time):
+    response = make_404(initial_request, date=expected_time)
     time = response.get('time')
-    assert time == TIME
+    assert time == expected_time
 
 
-def test_data_make_404():
-    response = make_404(REQUEST, date=TIME)
+def test_data_make_404(initial_request, expected_time):
+    response = make_404(initial_request, date=expected_time)
     data = response.get('data')
-    assert data == f'Action "{REQUEST.get("action")}" not found'
+    assert data == f'Action "{initial_request.get("action")}" not found'
 
 
-def test_code_make_500():
-    response = make_500(REQUEST, date=TIME)
+def test_code_make_500(initial_request, expected_time):
+    response = make_500(initial_request, date=expected_time)
     code = response.get('code')
     assert code == 500
 
 
-def test_time_make_500():
-    response = make_500(REQUEST, date=TIME)
+def test_time_make_500(initial_request, expected_time):
+    response = make_500(initial_request, date=expected_time)
     time = response.get('time')
-    assert time == TIME
+    assert time == expected_time
 
 
-def test_data_make_500():
-    response = make_500(REQUEST, date=TIME)
+def test_data_make_500(initial_request, expected_time):
+    response = make_500(initial_request, date=expected_time)
     data = response.get('data')
     assert data == 'Internal server error'
